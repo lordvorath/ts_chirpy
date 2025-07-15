0b326e24f8b54ae7c5a351f7b971fdbafefe865f
lordvorath <4kill3@gmail.com>
2025-07-15 11:59:17 +0000

    serve static files

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import path from "path";
 
+async function handlerReadiness(req: express.Request, res: express.Response): Promise<void>{
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.send("OK");
+}
+
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
