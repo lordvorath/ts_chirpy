f2b4cba11f9cb67bd322b071890d21630ee4bb14
lordvorath <4kill3@gmail.com>
2025-07-16 09:17:59 +0000

    logging middleware

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import express from "express";
+import { STATUS_CODES } from "http";
 import path from "path";
+
+function middlewareLogResponses(req: express.Request, res: express.Response, next: express.NextFunction): void {
+    res.on("finish", () => {
+        if (res.statusCode >= 300) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+        }
+    });
+    next();
+}
 
 async function handlerReadiness(req: express.Request, res: express.Response): Promise<void>{
     res.setHeader("Content-Type", "text/plain; charset=utf-8");
@@ -9,6 +19,7 @@ async function handlerReadiness(req: express.Request, res: express.Response): Pr
 const app = express();
 const PORT = 8080;
 
+app.use(middlewareLogResponses);
 app.use("/app", express.static("./src/app"));
 
 app.get("/healthz", handlerReadiness);
